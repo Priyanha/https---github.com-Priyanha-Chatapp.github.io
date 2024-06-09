77e682c3541c7a4cb11accefede039187526f7a7
Priyanha <priya.param6@gmail.com>
2024-06-10 03:43:15 +1000

    changes

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/secure-chat-app',
+  mongoURI: process.env.MONGO_URI || 'mongodb+srv://treetest28:treetest28@cluster0.y4zxtwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/secure-chat-app',
   secretOrKey: process.env.JWT_SECRET || 'your_secret_key',
   emailUser: process.env.EMAIL || 'mynewsocial12345@gmail.com',
   emailPassword: process.env.EMAIL_PASSWORD || 'vkqo qafe aoqu jrfa',
